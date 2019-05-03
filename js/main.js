ed43f99a295f6b46b700bf15a2edcb58a4fdb8fb
Marcel Heberer <marcel@dieheberers.de>
2019-05-03 14:24:30 +0200

    Initialize repository

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var myFullpage = new fullpage('#fullpage', {
+    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+    scrollBar: false,
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Hi', 'Was geht'],
+});
